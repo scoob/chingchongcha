9781d023af5a7f12064e7dd0ad7ef23fc1bec3f1
Simon Scheepers <simon.scheepers@mks-1067.internal.meerkats.com.au>
2016-04-28 14:03:03 +0800

    install base libraries and update gulp sass config

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,12 @@ var gulp = require("gulp"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify");
+  sass = require("gulp-sass"),
+  bless = require("gulp-bless"),
+  minify = require("gulp-minify"),
+  autoprefixer = require("gulp-autoprefixer"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename");
 
 var webroot = "./wwwroot/";
 
@@ -13,9 +18,11 @@ var paths = {
   js: webroot + "js/**/*.js",
   minJs: webroot + "js/**/*.min.js",
   css: webroot + "css/**/*.css",
+  scss: "Stylesheets/*.scss",
+  sassDest: webroot + "css",
+  cssDest: webroot + "css",
   minCss: webroot + "css/**/*.min.css",
-  concatJsDest: webroot + "js/site.min.js",
-  concatCssDest: webroot + "css/site.min.css"
+  concatJsDest: webroot + "js/site.min.js"
 };
 
 gulp.task("clean:js", function (cb) {
@@ -24,6 +31,22 @@ gulp.task("clean:js", function (cb) {
 
 gulp.task("clean:css", function (cb) {
   rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task('images', function () {
+  return gulp.src(config.files.images)
+    .pipe(gulp.dest(config.dirs.images.dest));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass())
+    .pipe(autoprefixer('last 3 version', '> 1%'))
+    .pipe(gulp.dest(paths.sassDest))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(bless())
+    .pipe(gulp.dest(paths.cssDest));
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
@@ -44,4 +67,4 @@ gulp.task("min:css", function () {
     .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task("min", ["min:js", "sass"]);
