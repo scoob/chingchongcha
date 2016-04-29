6e1da35baeb5830067876b115631b911b6a45304
Simon Scheepers <simon@ecal.com>
2016-04-30 00:27:04 +0800

    add rounds

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,13 @@
 const angular = require('angular');
+const round = require('./round');
+const scoreboard = require('./scoreboard');
+const ngStorage = 'ngStorage'; require('ngstorage');
 
-angular.module('app', [])
+angular.module('app', [
+  ngStorage,
+  round,
+  scoreboard
+])
 .controller('AppController', function () {
   const controller = this;
   controller.test = 'Hello';
